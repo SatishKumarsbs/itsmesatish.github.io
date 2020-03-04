721f285e65c32757de118870627c936ec3b00c36
Satish Kumar <43923694+SatishKumarsbs@users.noreply.github.com>
2020-03-04 12:30:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,88 @@
+window.sr=ScrollReveal();
+sr.reveal('.animate-top',{
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom-1',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1200
+});
+
+sr.reveal('.animate-bottom-2',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1400
+});
+
+sr.reveal('.animate-bottom-3',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1600
+});
+
+sr.reveal('.animate-bottom-4',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1800
+});
+sr.reveal('.animate-bottom-5',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 2000
+});
+
+sr.reveal('.animate-bottom',{
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-right-1',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1200
+});
+
+sr.reveal('.animate-right-2',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1400
+});
+
+sr.reveal('.animate-right-3',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1600
+});
+
+sr.reveal('.animate-right-4',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 1800
+});
+sr.reveal('.animate-right-5',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 2000
+});
+sr.reveal('.animate-right-6',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 2200
+});
